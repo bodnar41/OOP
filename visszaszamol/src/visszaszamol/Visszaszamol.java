9c6cdb70e3454f1a901149336c3b059d3525520b
Márk <Mrk@DESKTOP-SDTJE3S.local>
2018-02-19 20:33:44 +0100

    first

---
+++ visszaszamol/src/visszaszamol/Visszaszamol.java
@@ -0,0 +1,13 @@
+/* Azaz kiírja a számokat 10-tól 1-ig, majd kiírja hogy Bumm! */
+package visszaszamol;
+
+public class Visszaszamol {public static void main(String[] args) {
+	for (int i = 10; i > 0; i--) {
+		System.out.println(i);
+	}
+	
+	System.out.println("Bumm!");
+
+}
+
+}
