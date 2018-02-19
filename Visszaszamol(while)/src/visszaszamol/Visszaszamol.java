de6d7ae6f42acea6eaffbd61408b9c971b3ec652
Márk <Mrk@DESKTOP-SDTJE3S.local>
2018-02-19 20:35:52 +0100

    first

---
+++ Visszaszamol(while)/src/visszaszamol/Visszaszamol.java
@@ -0,0 +1,16 @@
+
+/* Azaz kiírja a számokat 10-tól 1-ig, majd kiírja hogy Bumm!(while) */
+package visszaszamol;
+
+public class Visszaszamol {
+	public static void main (String [] args) {
+		int i=10;
+		
+		while(i>0) {
+			System.out.println(i);
+			i--;
+		}
+		System.out.println("Bumm");
+	}
+
+}
