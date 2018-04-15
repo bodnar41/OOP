abfa87cc59dfc630c566c2a97fb50dd7099f65f0
Márk <Mrk@Marko.local>
2018-04-15 20:49:05 +0200

    first

---
+++ Produuct/src/szinezheto/ISzinezheto.java
@@ -0,0 +1,11 @@
+package szinezheto;
+
+import java.awt.Color;
+
+public interface ISzinezheto {
+	public Color defaultColor = Color.red;
+
+	public void setColor(Color colorIn);
+
+	public Color getColor();
+}
